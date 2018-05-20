a09d0f3be778eff4b887c8cd44043dbf65394cfd
Prakhar-FF13 <prakhar.meerut@gmail.com>
2018-05-20 19:45:19 +0530

    Added Delete Functionality and Fix Code Error.

--- Todo List using JavaScript/list.js
+++ Todo List using JavaScript/list.js
@@ -3,12 +3,27 @@ var todos = [];
 var input = prompt("What would you like to do ?");
 
 while(input !== "Quit"){
-	if(input === "List")
-		console.log(todos);
+	
+	if(input === "List"){
+			console.log("****************");
+			todos.forEach(function(todo, i){
+				console.log(i + ": " + todo);
+			});
+			console.log("****************");
+	}
+	
 	else if(input === "New"){
 		var a = prompt("Enter a new todo ");
-		input.push(a);
+		todos.push(a);
+		console.log("Item added");
 	}
+	
+	else if(input === "Delete"){
+		var todel = prompt("Enter the index to todo to be deleted ");
+		todos.splice(todel,1);
+		console.log("Item deleted");
+	}
+	
 	var input = prompt("What would you like to do ?");
 }
 console.log("You quit the app !!");
