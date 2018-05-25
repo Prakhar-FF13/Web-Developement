2f0064b06b35a570587e10d39b4043fe3052533e
Prakhar-FF13 <prakhar.meerut@gmail.com>
2018-05-25 17:44:11 +0530

    Styling the TodoList using FontAwesome and CSS and CSS Animations.

--- TodoList using JQuery/assets/js/todos.js
+++ TodoList using JQuery/assets/js/todos.js
@@ -13,6 +13,10 @@ $("input[type='text']").keypress(function(event){
 	if(event.which == 13){
 		var todotext = $(this).val();
 		$(this).val("");
-		$("ul").append("<li><span>X </span>" + todotext + "</li>");
+		$("ul").append("<li><span><i class='fa fa-trash'></i> </span>" + todotext + "</li>");
 	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
 });
